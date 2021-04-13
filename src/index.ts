02be6a27e2ff7827d432f5ba9ba94fd63e6590ad
Your Name <you@example.com>
2021-04-13 22:28:11 +0900

    first

--- src/index.ts
+++ src/index.ts
@@ -2,47 +2,65 @@
 import * as createjs from 'createjs-module';
 import * as Matter from 'matter-js';
 window.addEventListener("load", init);
+const MAP=[
+[1,1,1,1,1,1],
+[1,0,0,0,0,1],
+[1,0,0,0,0,1],
+[1,0,0,0,0,1],
+[1,0,0,0,0,1],
+[1,1,1,1,1,1]
+];
 var stage = new createjs.Stage("myCanvas");
 var a:number;
 var Engine:Matter.Engine;
 var World:Matter.World;
-var floor:Matter.Body;
 var ball:Matter.Body;
+var meiro:Matter.Body[];
 function init(){
     stage = new createjs.Stage("myCanvas");
     a=0;
-    gameinit();
     console.log("init");
+    meiro=new Array(0);
+    gameinit();
     //createjs.Ticker.addEventListener("tick", handleTick);
 }
 function gameinit(){
     Engine=Matter.Engine.create();
     World=Engine.world;
-    floor = Matter.Bodies.rectangle(170, 300, 600, 20, {
-        isStatic: true, 
-        id: 100,
-        friction:100,
-        angle:3.28
-    });
     ball = Matter.Bodies.circle(200, 0,100,{
         isStatic: false, 
-        id: 1,
-        friction:100
+        friction:1
     });
-    Matter.World.add(Engine.world,floor);
     Matter.World.add(Engine.world,ball);
+    var cnt=0;
+    for(var i=0;i<MAP.length;i++){
+        for(var j=0;j<MAP[i].length;j++){
+            if(MAP[i][j]==1){
+                var bd=Matter.Bodies.rectangle(100+i*50, 400+j*50, 50, 50, {
+                    isStatic: true, 
+                    friction:1,
+                    angle:100
+                });
+                Matter.World.add(Engine.world,bd);
+            meiro.push(bd);
+        }
+        }
+    }
     Matter.Events.on(Engine, 'afterUpdate', function () {
         handleTick();
     });
     Matter.Engine.run(Engine);
 }
 function handleTick(){
+    var container = new createjs.Container();
+    stage.addChild(container);
     var shape = new createjs.Shape();
     shape.graphics.beginFill("DarkRed"); // 赤色で描画するように設定
     shape.graphics.drawCircle(ball.position.x, ball.position.y, ball.circleRadius); //半径100pxの円を描画
     console.log("anglel:"+ball.angle);
-    stage.addChild(shape); // 表示リストに追加
-    const vertices = floor.vertices;
+    container.addChild(shape); // 表示リストに追加
+    meiro.forEach(Element=>{
+    const vertices = Element.vertices;
     var obj = new createjs.Shape();
     obj.graphics.beginFill("#EA6163");
     obj.graphics.beginStroke("#5984BE");
@@ -51,8 +69,9 @@ function handleTick(){
         obj.graphics.lineTo(vertices[j].x, vertices[j].y);
     }
     obj.graphics.lineTo(vertices[0].x, vertices[0].y);
-    stage.addChild(obj); // 表示リストに追加
+    container.addChild(obj); // 表示リストに追加
+    console.log("updating id "+Element.id);
+    });
     stage.update();
-    stage.removeChild(obj);
-    stage.removeChild(shape);
+    stage.removeChild(container);
 }
