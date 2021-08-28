c43cccba5116226059caac72a5b160b433ee3d59
Your Name <you@example.com>
2021-08-29 01:35:28 +0900

    t

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,29 @@
-
-import * as Matter from 'matter-js';
-window.addEventListener("load", init);
-const MAP=[
-[1,1,1,1,1,1],
-[1,0,0,0,0,1],
-[1,0,0,0,0,1],
-[1,0,0,0,0,1],
-[1,0,0,0,0,1],
-[1,1,1,1,1,1]
-];
-var a:number;
-var Engine:Matter.Engine;
-var World:Matter.World;
-var ball:Matter.Body;
-var meiro:Matter.Body[];
-function init(){
-    a=0;
-    console.log("init");
-    meiro=new Array(0);
-    gameinit();
-    //createjs.Ticker.addEventListener("tick", handleTick);
-}
-function gameinit(){
-    Engine=Matter.Engine.create();
-    World=Engine.world;
-    ball = Matter.Bodies.circle(200, 0,100,{
-        isStatic: false, 
-        friction:1
-    });
-    Matter.World.add(Engine.world,ball);
-    var cnt=0;
-    for(var i=0;i<MAP.length;i++){
-        for(var j=0;j<MAP[i].length;j++){
-            if(MAP[i][j]==1){
-                var bd=Matter.Bodies.rectangle(100+i*50, 400+j*50, 50, 50, {
-                    isStatic: true, 
-                    friction:1,
-                    angle:100
-                });
-                Matter.World.add(Engine.world,bd);
-            meiro.push(bd);
-        }
-        }
+import * as Phaser from 'phaser';
+import {GameScene} from './scenes/GameScene';
+window.addEventListener('load', () => {
+    const game = new Game(config);
+});
+const config: Phaser.Types.Core.GameConfig = {
+    //画面サイズ
+    width: 720,
+    height: 340,
+    type: Phaser.AUTO,
+    //ゲーム画面を描画するcanvasを書き出す先
+    parent: 'roguelike',
+    //ゲーム画面を伸縮して表示させるための設定
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        parent: 'game',
+    },
+    //あとでコメントアウトを解除する
+    //必要なシーンを読み込む
+   scene: [GameScene],
+};
+class Game extends Phaser.Game {
+    constructor(config: Phaser.Types.Core.GameConfig) {
+        super(config);
+        this.scene.start('GameScene');
     }
-    Matter.Events.on(Engine, 'afterUpdate', function () {
-        
-    });
-    Matter.Engine.run(Engine);
 }
+
